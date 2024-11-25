a11db1dfaf942fd3d380b501f9c4609bf7d33fcb
PhapNguyenHoangViet <vietphappro1@gmail.com>
2024-11-25 10:34:47 +0700

    Update all
    Update all

--- app/movie/gcn_model.py
+++ app/movie/gcn_model.py
@@ -10,31 +10,39 @@ MODEL_PATH = os.path.join(BASE_DIR, 'movie', 'gcn_model.pth')
 class GCN(torch.nn.Module):
     def __init__(self, num_features, hidden_channels, num_users, num_items):
         super(GCN, self).__init__()
+        # Thêm nhiều lớp convolution
         self.conv1 = GCNConv(num_features, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, num_items)
-        self.dropout = torch.nn.Dropout(p=0.5)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels // 2)
+        self.conv3 = GCNConv(hidden_channels // 2, num_items)
+        self.dropout = torch.nn.Dropout(0.5)
+        
+        # Embedding với regularization
         self.user_embeddings = torch.nn.Embedding(num_users, hidden_channels)
         self.item_embeddings = torch.nn.Embedding(num_items, hidden_channels)
-
+        
+    
     def forward(self, x, edge_index):
-        x = F.relu(self.conv1(x, edge_index))
+        x = F.relu((self.conv1(x, edge_index)))
         x = self.dropout(x)
-        x = self.conv2(x, edge_index)
+        x = F.relu((self.conv2(x, edge_index)))
+        x = self.dropout(x)
+        x = self.conv3(x, edge_index)
         return x
-
+    
+    
     def recommend(self, user_id, k=5):
         self.eval()
         with torch.no_grad():
             user_embedding = self.user_embeddings(torch.tensor([user_id], dtype=torch.long))
             item_embeddings = self.item_embeddings.weight
             item_scores = torch.matmul(user_embedding, item_embeddings.t())
-            _, top_items = torch.topk(item_scores, k)
-            return top_items.squeeze().tolist()
+            _, sorted_indices = torch.topk(item_scores, k)
+            return sorted_indices.squeeze().tolist()
 
 
 def load_model():
     num_features = 50
-    hidden_channels = 32
+    hidden_channels = 64
     num_users = 943
     num_items = 1682
 
@@ -48,4 +56,4 @@ def load_model():
 def recommend_movies(user_id=1, k=5):
     model = load_model()
     recommended_item_ids = model.recommend(user_id, k)
-    return list(recommended_item_ids)
+    return recommended_item_ids
